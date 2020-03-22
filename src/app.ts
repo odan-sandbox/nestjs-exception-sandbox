2940e2fe97e9db066a1b5b7db9c3ef7a3069280f
odan <odan3240@gmail.com>
2020-03-23 03:02:31 +0900

    chore(nest): setup

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,12 @@ export function add(x: number, y: number): number {
   return x + y;
 }
 
-console.log("poyo");
+import { NestFactory } from "@nestjs/core";
+
+import { AppModule } from "./app.module";
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
