82b459ce7e4845e1da5a91d140f11332e72ce49d
odan <odan3240@gmail.com>
2020-03-24 03:22:08 +0900

    コメントを追加

--- src/exception-filter.ts
+++ src/exception-filter.ts
@@ -3,6 +3,8 @@ import { Response } from "express";
 
 import { BaseError } from "./error";
 
+// @Catch で指定したクラスは instanceof で比較される
+// https://github.com/nestjs/nest/blob/23f477c73f3de2681498def0f041c3a42b7374b6/packages/core/exceptions/exceptions-handler.ts#L35
 @Catch(BaseError)
 export class CustomExceptionFilter implements ExceptionFilter<BaseError> {
   catch(exception: BaseError, host: ArgumentsHost): void {
